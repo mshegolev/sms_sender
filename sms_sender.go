9d29e744738844b0ca26cead2be28c236ca2b8cb
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2020-10-16 19:19:22 +0300

    init project

---
+++ sms_sender.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"strings"
+
+	requests "github.com/asmcos/requests"
+)
+
+func main() {
+	apiID := "changeMe!"
+	message := "Hello world!"
+	phoneNumber := "9211232323"
+	smsURL := "https://sms.ru/sms/send?api_id=key&to=number&msg=message&json=1"
+	smsURL = strings.Replace(smsURL, "key", apiID, 1)
+	smsURL = strings.Replace(smsURL, "number", phoneNumber, 1)
+	smsURL = strings.Replace(smsURL, "message", message, 1)
+	resp, err := requests.Get(smsURL)
+	if err != nil {
+		return
+	}
+	println(resp.Text())
+}
